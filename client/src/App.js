bbc32985549998355d8f477d3e97428ba8381da6
Nitya Tiwari <nityasdl002@gmail.com>
2024-01-09 12:10:31 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useRoutes } from 'react-router-dom';
+import Router from './routes/Router';
+
+import { baselightTheme } from "./theme/DefaultColors";
 
 function App() {
+  const routing = useRoutes(Router);
+  const theme = baselightTheme;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+
+      <CssBaseline />
+      {routing}
+
+    </ThemeProvider>
   );
 }
 
