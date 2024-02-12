26c4880f096d522d4a8f07de6f2bd71f8c1eb072
Nitya Tiwari <nityasdl002@gmail.com>
2024-02-12 12:18:05 +0530

    updated models

--- server/models/chatModel.js
+++ server/models/chatModel.js
@@ -7,7 +7,7 @@ const chatModel = mongoose.Schema(
         users : [{
             type : mongoose.Schema.Types.ObjectId,
             ref : "User",
-        },
+        }
         ],
         latestMessage : {
             type : mongoose.Schema.Types.ObjectId,
