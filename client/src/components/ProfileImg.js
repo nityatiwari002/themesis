3bbfc3219a31534affb1be2ffa8037face0ec4cc
Shreya Singh <singh.shreya0511@gmail.com>
2024-02-24 12:57:12 +0530

    added modal for group details and resolve image fetching issue

--- client/src/components/ProfileImg.js
+++ client/src/components/ProfileImg.js
@@ -4,7 +4,9 @@ function ProfileImg({ person }) {
 	return (
 		<>
 			<img
-				src={`http://localhost:5001/uploads/${person.image}`}
+				// src={`http://localhost:5001/uploads/${person.image}`}
+				src = {person.image === "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" ? person.image : `http://localhost:5001/uploads/${person.image}`}
+
 				className="lawyer-image"
 			></img>
 		</>
